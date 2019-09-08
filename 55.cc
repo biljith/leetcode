5eb1f57d72500b601cf308d86f34a70f9441981b
biljith <51287228+biljith@users.noreply.github.com>
2019-09-08 00:12:04 -0600

    Update 55.cc

--- 55.cc
+++ 55.cc
@@ -3,18 +3,12 @@ class Solution {
 public:
     
     bool canJump(vector<int>& nums) {
-        vector<bool> dp(nums.size(), false);
-        dp[nums.size() - 1] = true;
-        for (int i = nums.size() - 2; i >=0; --i) {
-            int furthest = std::min(i + nums[i], (int)nums.size() - 1);
-            for (int j = i + 1; j <= furthest ; ++j) {
-                if (dp[j] == true) {
-                    dp[i] = true;
-                    break;
-                }    
+        int last_good = nums.size() - 1;
+        for (int i = nums.size() - 2; i >= 0; --i) {
+            if (i + nums[i] >= last_good) {
+                last_good = i;
             }
         }
-        if (dp[0] == true) return true;
-        else return false;
+        return last_good == 0;
     }
 };
